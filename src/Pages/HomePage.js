3992bb3a562e8f488b0f4466d331c499e740e63f
stefanusdeo <stefanusdeo07@gmail.com>
2021-10-18 08:46:07 +0700

    penambahan alert

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -77,6 +77,7 @@ const HomePage = () => {
           }
         }
         setPokemon(dbPokemon);
+        alert('Yesss Dapatt!!!');
         localStorage.setItem('dbPokemon', JSON.stringify(dbPokemon));
         setLoading(false);
       }
