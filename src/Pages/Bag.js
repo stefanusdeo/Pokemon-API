9e44d12396821cafcc8b5fe6a861a832daf95d26
stefanusdeo <stefanusdeo07@gmail.com>
2023-03-08 11:40:06 +0700

    fixing bag

--- src/Pages/Bag.js
+++ src/Pages/Bag.js
@@ -1,20 +1,25 @@
-import { faBackward } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useState, useEffect } from 'react';
-import { Row, Col } from 'react-bootstrap';
-import ListBag from '../Components/ListBag';
+import { faBackward } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState, useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import ListBag from "../Components/ListBag";
 
-import Loader from '../Components/Loader';
+import Loader from "../Components/Loader";
 
 const Bag = () => {
   const [loading, setLoading] = useState(true);
   const [pokemon, setPokemon] = useState([]);
 
   const getPokemon = () => {
-    const localBag = localStorage.getItem('dbBag');
-    const dbPokemon = JSON.parse(localBag);
-    setPokemon(dbPokemon);
-    setLoading(false);
+    const localBag = localStorage.getItem("dbBag");
+    if (localBag) {
+      const dbPokemon = JSON.parse(localBag);
+      setPokemon(dbPokemon);
+      setLoading(false);
+    } else {
+      setPokemon([]);
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -26,20 +31,20 @@ const Bag = () => {
       {loading ? (
         <Loader />
       ) : (
-        <div className='text-center'>
+        <div className="text-center">
           <h1>Isi Tas</h1>
-          <div className='d-flex justify-content-end'>
+          <div className="d-flex justify-content-end">
             <button
-              className='btn btn-sm btn-secondary'
+              className="btn btn-sm btn-secondary"
               onClick={() => window.history.back()}
             >
-              <FontAwesomeIcon icon={faBackward} className='me-2' /> Back
+              <FontAwesomeIcon icon={faBackward} className="me-2" /> Back
             </button>
           </div>
           <Row>
             {pokemon.map((res) => (
-              <Col key={res.data.id} xs={12} sm={12} md={4}>
-                <ListBag pokemon={res.data} />
+              <Col key={res?.data?.id} xs={12} sm={12} md={4}>
+                <ListBag pokemon={res?.data} />
               </Col>
             ))}
           </Row>
